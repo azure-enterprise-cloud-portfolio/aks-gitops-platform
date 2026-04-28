2e4a1dd2255fce25e09b45b405dd34d1e23655a2
Nurunnubi <talukder9712@gmail.com>
2026-04-28 18:43:20 -0400

    fix: terraform fmt versions.tf

--- terraform/envs/platform/versions.tf
+++ terraform/envs/platform/versions.tf
@@ -7,7 +7,7 @@ terraform {
     resource_group_name  = "rg-cs-tfstate-cac"
     storage_account_name = "stcstfstatecac001"
     container_name       = "tfstate"
-    key                  = "platform/terraform.tfstate"  # fixed — was pointing to dev
+    key                  = "platform/terraform.tfstate" # fixed — was pointing to dev
   }
 
   required_providers {
